f62c0b6ec790e875ea39812f4858466fa69b2047
Jen Stewart <jstewart8053@gmail.com>
2021-07-10 20:47:30 -0500

    added backup db script

---
+++ server/dbScript.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS employeesystem(
+	Name varchar(55) NOT NULL,
+	Age int NOT NULL,
+	Country VARCHAR(55) NOT NULL,
+	Position VARCHAR(55) NOT NULL,
+	Wage int NOT NULL,
+	
+)
--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   user: "root",
   host: "localhost",
-  password: "password",
+  password: "Newpass1",
   database: "employeesystem",
 });
 
